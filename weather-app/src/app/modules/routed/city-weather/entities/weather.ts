e31293c9805dd30f3cf8e9cd7a04484332f6867c
Manuel Cañero Sancho <manuelcasancho@gmail.com>
2018-01-12 12:32:57 +0000

    added changes displaying the temperature, modified the README file

--- weather-app/src/app/modules/routed/city-weather/entities/weather.ts
+++ weather-app/src/app/modules/routed/city-weather/entities/weather.ts
@@ -1,13 +1,13 @@
 export class Weather {
     dt_txt: string;
-    temp: number;
+    temp: string;
 
     constructor(data: any = {}) {
         this.dt_txt = data.dt_txt;
         this.temp = data.main ? this.tempInCelsius(data.main.temp) : null;
     }
 
-    public tempInCelsius = (temp: number): number => {
+    public tempInCelsius = (temp: number): string => {
         let tempInCelsius = null;
         if (this.temp !== null) {
             tempInCelsius = this.kelvinToCelsius(temp);
@@ -15,7 +15,7 @@ export class Weather {
         return tempInCelsius;
     }
 
-    private kelvinToCelsius = (kelvin: number): number => {
-        return Math.round(kelvin - 273.15);
+    private kelvinToCelsius = (kelvin: number): string => {
+        return Math.round(kelvin - 273.15) + ' C';
     }
 }
